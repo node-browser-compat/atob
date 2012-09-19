543a61a94176956c9e7b957c3d7abcee3898d664
AJ ONeal <coolaj86@gmail.com>
2012-09-19 14:22:55 -0600

    handle actual binary, not just utf8

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
+/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
 (function () {
   "use strict";
 
   var atob = require('./index')
     , encoded = "SGVsbG8gV29ybGQ="
     , unencoded = "Hello World"
-    , result
+  /*
+    , encoded = "SGVsbG8sIBZM"
+    , unencoded = "Hello, 世界"
+  */
     ;
 
   if (unencoded !== atob(encoded)) {
+    console.log('[FAIL]', unencoded, atob(encoded));
     return;
   }
 
